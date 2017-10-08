99f67fc17742490953f59715c7f5b75956029a06
Jay Shenoy <jay@jayshenoy.com>
2017-10-08 02:57:51 -0700

    Include config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+MONGODB_NAME = 'bearpay'
+MONGODB_URI = 'mongodb://admin:password@ds113835.mlab.com:13835/bearpay'
+
+C1_API_KEY = '2f40dbc02063cfe80495c674ae62e349'
+SPARKPOST_API_KEY = '5880cde7a953f9360085664d71df16f5eb55778b'
+SPARKPOST_TEMPLATE_ID = 'bear-pay'
